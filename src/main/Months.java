060552c7ae8ae33b0cc89bc45fc7f1ee8c8a90b0
Lenovo <priyanshi.jaiswal_cs19@gla.ac.in>
2020-10-27 21:05:03 +0530

    Using getter method shows total number of days in months.

--- src/main/Months.java
+++ src/main/Months.java
@@ -1,16 +1,26 @@
 package main;
 
 public enum Months {
-    JANUARY,
-    FEBRUARY,
-    MARCH,
-    APRIL,
-    MAY,
-    JUNE,
-    JULY,
-    AUGUST,
-    SEPTEMBER,
-    OCTOBER,
-    NOVEMBER,
-    DECEMBER
+    JANUARY(31),
+    FEBRUARY(28),
+    MARCH(31),
+    APRIL(30),
+    MAY(31),
+    JUNE(30),
+    JULY(31),
+    AUGUST(31),
+    SEPTEMBER(30),
+    OCTOBER(31),
+    NOVEMBER(30),
+    DECEMBER(31);
+
+    private final int totalNumberOfDays;
+
+    Months(int totalNumberOfDays) {
+        this.totalNumberOfDays = totalNumberOfDays;
+    }
+
+    public int getTotalNumberOfDays() {
+        return totalNumberOfDays;
+    }
 }
